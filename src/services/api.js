997ee795e94adbb401824648e7b7088e3c58fd35
Abu-Hafsah <najib.mahmud@student.moringaschool.com>
2026-07-22 17:26:24 +0300

    Add JWT interceptor for authenticated API requests

--- src/services/api.js
+++ src/services/api.js
@@ -7,4 +7,15 @@ const api = axios.create({
   },
 });
 
+// Automatically attach JWT token to every request
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default api;
